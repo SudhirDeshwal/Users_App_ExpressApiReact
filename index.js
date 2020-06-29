7c7f142fe49a68873aef2f99b7e1e6d457ad7680
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-06-29 19:18:19 -0400

    path and fs used

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
 
-    res.write('<h1>Hi sudhir1</h1>');
+    if (req.url ==='/') {
+    res.write('<h1>Hi sudhir</h1>');
     res.end();
+    }
+    if (req.url === '/about') {
+
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+            (err, content) => {
+
+                if (err) throw err;
+                res.writeHead(200, {'Content-Type' : 'text/html'});
+                res.end(content);
+            }
+
+
+        );
+    }
+
+    if (req.url === '/api/users') {
+        const users = [
+          { name: 'bob', age: 23 },
+          { name: 'bob3', age: 33 },
+        ];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(users));
+      }
 
 });
 
