36612d7531bb013a1c31c33df16f5072c6206ddb
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-06-29 18:45:18 -0400

    all files added

---
+++ data/todo.js
@@ -0,0 +1,19 @@
+let todoslist = [
+  {
+    id: '1',
+    title: 'todo 1',
+    done: true,
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    done: false,
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    done: true,
+  },
+];
+
+module.exports = todoslist;
