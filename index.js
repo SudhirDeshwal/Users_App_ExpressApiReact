9a5b3717d2081cb9082b3719c811cbb4fc5bc99e
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-06-29 21:12:32 -0400

    file clean up done

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const server = http.createServer((req, res) => {
     res.write('<h1>Hi sudhir</h1>');
     res.end();
     }
-    if (req.url === '/about') {
+    // if (req.url === '/about') {
 
-        fs.readFile(
-            path.join(__dirname, 'public', 'about.html'),
-            (err, content) => {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'about.html'),
+    //         (err, content) => {
 
-                if (err) throw err;
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                res.end(content);
-            }
+    //             if (err) throw err;
+    //             res.writeHead(200, {'Content-Type' : 'text/html'});
+    //             res.end(content);
+    //         }
 
 
     //     );
